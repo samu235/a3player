49558bab428b742f45a0572b6006f8a483729d70
samu235 <samuel2351994@hotmail.es>
2022-08-15 11:22:57 +0200

    add posibility of cancel evente prevent

--- components/button/Button.js
+++ components/button/Button.js
@@ -2,22 +2,30 @@ import styles from './Button.module.css'
 
 export default function Button(props) {
 
-    let addStyles = (props?.color === 'red' )? styles.cancel : (props?.color === 'green' )? styles.accept :""
+    let addStyles = (props?.color === 'red') ? styles.cancel : (props?.color === 'green') ? styles.accept : ""
 
-    function newOnClick(event){
-        event.preventDefault()
-        props?.onClick()
+    function newOnClick(event) {
+        if(!props.hasOwnProperty('cancelEventPrevent')){
+            event.preventDefault()
+        }
+        if (props.onClick) {
+            props?.onClick()
+        }
     }
-    function newOnDoubleClick(event){
-        event.preventDefault()
-        props?.onDoubleClick()
+    function newOnDoubleClick(event) {
+        if(!props.hasOwnProperty('cancelEventPrevent')){
+            event.preventDefault()
+        }
+        if (props?.onDoubleClick) {
+            props?.onDoubleClick()
+        }
     }
     return (
         <button
             type={props?.type}
             className={props?.className + " " + styles.btn + " " + addStyles}
-            onClick={(event)=>newOnClick(event)}
-            onDoubleClick={(event)=>newOnDoubleClick(event)}
+            onClick={(event) => newOnClick(event)}
+            onDoubleClick={(event) => newOnDoubleClick(event)}
         >
             {props?.children}
         </button>
