e4296182709f2d5ad32cd3edf5b84e2f481136df
samu235 <samuel2351994@hotmail.es>
2022-08-15 11:23:55 +0200

    change link to detail page of all div to button

--- components/itemList/ItemList.js
+++ components/itemList/ItemList.js
@@ -8,16 +8,18 @@ import styles from './ItemList.module.css'
 export default function ItemList(props) {
 
     return (
-        <Link href={"/site/"+props?.item?._id}>
-            <a className={styles.aDefault}>
-                <div className={styles.container}>
-                    <div className={styles.name}>{props?.item?.name}</div>
-                    <ModalCreateItem item={props?.item} />
-                    <ModalDeleteItem item={props?.item} />
 
-                </div>
-            </a>
-        </Link>
+        <div className={styles.container}>
+            <div className={styles.name}>{props?.item?.name}</div>
+            <ModalCreateItem item={props?.item} />
+            <ModalDeleteItem item={props?.item} />
+            <Link href={"/site/" + props?.item?._id}>
+                <a className={styles.aDefault}>
+                    <Button cancelEventPrevent>Detalle</Button>
+                </a>
+            </Link>
+        </div >
+
 
     )
 }
