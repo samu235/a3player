68547afc67cdfdf4deddefa6dd6f22b925417aae
samu235 <samuel2351994@hotmail.es>
2022-08-16 02:14:38 +0200

    improves eyesight of site pages

--- pages/site/[id].js
+++ pages/site/[id].js
@@ -16,40 +16,42 @@ export default function Menu() {
 
     return (
         <div className={styles.container}>
-            <div className={styles.title}>
-                <h2>{site.name}</h2>
-            </div>
-            <div className={styles.row}>
-                <div className={styles.name}>Name: </div>
-                <div className={styles.text}>{site.name}</div>
-            </div>
-            <div className={styles.row}>
-                <div className={styles.name}>Id: </div>
-                <div className={styles.text}>{site._id}</div>
-            </div>
-            <div className={styles.row}>
-                <div className={styles.name}>Description: </div>
-                <div className={styles.text}>{site.description}</div>
-            </div>
-            <div className={styles.row}>
-                <div className={styles.name}>Key: </div>
-                <div className={styles.text}>{site.key}</div>
-            </div>
-            <div className={styles.row}>
-                <div className={styles.name}>Path: </div>
-                <div className={styles.text}>{site.path}</div>
-            </div>
-            <div className={styles.row}>
-                <div className={styles.name}>PublicPath: </div>
-                <div className={styles.text}>{site.publicPath}</div>
-            </div>
-            <div className={styles.row}>
-                <div className={styles.name}>Site: </div>
-                <div className={styles.text}>{site.site}</div>
-            </div>
-            <div className={styles.row}>
-                <div className={styles.name}>CreateDate: </div>
-                <div className={styles.text}>{site.createDate}</div>
+            <div className={styles.containerChildren}>
+                <div className={styles.title}>
+                    <h2>{site.name}</h2>
+                </div>
+                <div className={styles.row}>
+                    <div className={styles.name}>Name: </div>
+                    <div className={styles.text}>{site.name}</div>
+                </div>
+                <div className={styles.row}>
+                    <div className={styles.name}>Id: </div>
+                    <div className={styles.text}>{site._id}</div>
+                </div>
+                <div className={styles.row}>
+                    <div className={styles.name}>Description: </div>
+                    <div className={styles.text}>{site.description}</div>
+                </div>
+                <div className={styles.row}>
+                    <div className={styles.name}>Key: </div>
+                    <div className={styles.text}>{site.key}</div>
+                </div>
+                <div className={styles.row}>
+                    <div className={styles.name}>Path: </div>
+                    <div className={styles.text}>{site.path}</div>
+                </div>
+                <div className={styles.row}>
+                    <div className={styles.name}>PublicPath: </div>
+                    <div className={styles.text}>{site.publicPath}</div>
+                </div>
+                <div className={styles.row}>
+                    <div className={styles.name}>Site: </div>
+                    <div className={styles.text}>{site.site}</div>
+                </div>
+                <div className={styles.row}>
+                    <div className={styles.name}>CreateDate: </div>
+                    <div className={styles.text}>{site.createDate}</div>
+                </div>
             </div>
         </div>
     )
